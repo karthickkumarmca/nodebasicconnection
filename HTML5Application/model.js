be30737c8ecd09543200f391915f2627e895e5ae
BHUVI <BHUVI@BHUVI-PC>
2019-03-31 19:21:47 +0530

    new-commit2

--- HTML5Application/model.js
+++
@@ -1,7 +0,0 @@
-exports.getdetails = function(connection,callback){
-	connection.get('user').find({},function(err,result){
-		if(err) console.log('connection', err);
-		else
-			callback(result);
-	})
-}
